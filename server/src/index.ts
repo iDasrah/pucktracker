668bee8b9c71f99faa48e9ddb41831e9553d005b
Mathéo <matheo.picouleau@hotmail.com>
2025-04-13 21:03:27 +0200

    ✨: ajout routes teams

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import env from "./config/env";
 import playerRouter from "./routes/player";
+import teamRouter from "./routes/team";
 
 export const app = express();
 const { PORT, API_ROOT, API_VERSION } = env;
@@ -11,6 +12,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(`${API_ROOT}/${API_VERSION}/players`,playerRouter);
+app.use(`${API_ROOT}/${API_VERSION}/teams`, teamRouter);
 
 app.get(`${API_ROOT}/${API_VERSION}`, (req, res) => {
   res.json({
