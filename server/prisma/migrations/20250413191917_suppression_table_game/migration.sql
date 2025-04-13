967541bd7fd18ea5ee40a99dfc8d820c3244875a
Mathéo <matheo.picouleau@hotmail.com>
2025-04-13 21:20:47 +0200

    🗃️: suppression table game

---
+++ server/prisma/migrations/20250413191917_suppression_table_game/migration.sql
@@ -0,0 +1,14 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Game` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropForeignKey
+ALTER TABLE "Game" DROP CONSTRAINT "Game_awayTeamId_fkey";
+
+-- DropForeignKey
+ALTER TABLE "Game" DROP CONSTRAINT "Game_homeTeamId_fkey";
+
+-- DropTable
+DROP TABLE "Game";
