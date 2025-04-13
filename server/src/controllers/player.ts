bce215a565dd94b97e199eb894b9be9ba435a4dd
Mathéo <matheo.picouleau@hotmail.com>
2025-04-13 21:02:54 +0200

    🔥: suppression routes inutiles

--- server/src/controllers/player.ts
+++ server/src/controllers/player.ts
@@ -14,7 +14,7 @@ export const getPlayerById = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
         const player = await prisma.player.findUnique({
-            where: { id: id },
+            where: { id: Number(id) },
         });
         if (!player) {
             res.status(404).json({ error: "Player not found" });
@@ -26,61 +26,11 @@ export const getPlayerById = async (req: Request, res: Response) => {
     }
 }
 
-export const createPlayer = async (req: Request, res: Response) => {
-    const { fullName, position, teamCode } = req.body;
-    try {
-        const player = await prisma.player.create({
-            data: {
-                fullName,
-                position,
-                Team: {
-                    connect: { teamCode: teamCode },
-                }
-            },
-        });
-        res.status(201).json(player);
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-}
-
-export const updatePlayer = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    const { fullName, position, teamCode } = req.body;
-    try {
-        const player = await prisma.player.update({
-            where: { id: id },
-            data: {
-                fullName,
-                position,
-                Team: {
-                    connect: { teamCode: teamCode },
-                }
-            },
-        });
-        res.status(200).json(player);
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-}
-
-export const deletePlayer = async (req: Request, res: Response) => {
-    const { id } = req.params;
-    try {
-        await prisma.player.delete({
-            where: { id: id },
-        });
-        res.status(204).send();
-    } catch (error) {
-        res.status(500).json({ error: "Internal Server Error" });
-    }
-}
-
 export const getPlayerStats = async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
         const playerStats = await prisma.playerStats.findMany({
-            where: { playerId: id },
+            where: { playerId: Number(id) },
         });
         res.status(200).json(playerStats);
     } catch (error) {
--- server/src/routes/player.ts
+++ server/src/routes/player.ts
@@ -1,12 +1,9 @@
 import { Router } from "express";
 
 import {
-    createPlayer,
-    deletePlayer,
     getAllPlayers,
     getPlayerById,
     getPlayerStats,
-    updatePlayer,
 } from "../controllers/player";
 
 const playerRouter = Router();
@@ -14,10 +11,6 @@ const playerRouter = Router();
 playerRouter.get("/", getAllPlayers);
 playerRouter.get("/:id", getPlayerById);
 playerRouter.get('/:id/stats', getPlayerStats);
-
-playerRouter.post("/", createPlayer);
-playerRouter.patch("/:id", updatePlayer);
-playerRouter.delete("/:id", deletePlayer);
 
 
 export default playerRouter;
