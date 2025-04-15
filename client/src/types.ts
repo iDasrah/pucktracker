68741854c0f4fa512eff2cd40d503c7f30d391a5
Mathéo <matheo.picouleau@hotmail.com>
2025-04-15 08:59:17 +0200

    🏷️: ajout types

--- client/src/types.ts
+++ client/src/types.ts
@@ -17,8 +17,22 @@ export type Game = {
 export type Player = {
     id: number;
     fullName: string;
+    teamCode: string;
     position: string;
     points: number;
     goals: number;
     assists: number;
+}
+
+export type PlayerStatsWithPlayer = {
+    playerId: number;
+    points: number;
+    goals: number;
+    assists: number;
+    player: {
+        id: number;
+        fullName: string;
+        teamCode: string;
+        position: string;
+    }
 };
