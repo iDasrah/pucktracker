67f48f88a27fb78bfbb38dab09f0c9e6ad4f28ab
Mathéo <matheo.picouleau@hotmail.com>
2025-04-15 10:53:30 +0200

    ✨: ajout filtrage getAllTeams

--- server/src/controllers/team.ts
+++ server/src/controllers/team.ts
@@ -1,9 +1,27 @@
 import { Request, Response } from "express";
 import prisma from "../services/db";
+import {Prisma} from "@prisma/client";
 
 export const getAllTeams = async (req: Request, res: Response) => {
+    const filter: Prisma.TeamWhereInput = {};
+    const { name } = req.query;
+
+    if (name) {
+        filter.name = {
+            contains: String(req.query.name),
+            mode: "insensitive",
+        };
+    }
+
     try {
-        const teams = await prisma.team.findMany();
+        const teams = await prisma.team.findMany(
+            {
+                where: filter,
+                orderBy: {
+                    name: "asc",
+                },
+            }
+        );
         res.status(200).json(teams);
     } catch (error) {
         res.status(500).json({ error: "Internal Server Error" });
